0c73f0dd57304e83575d3a7131d5bdd565b85e6e
Christopher Kerr <christopherke@truck-hero.com>
2020-04-27 17:59:41 -0400

    Lab 6

---
+++ Lab6/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Numerics;
+
+namespace Lab6
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            string response = "";
+            string input = "";
+
+            {
+                Console.Write("Welcome to the Grand Circus Casino! Roll the dice? (y/n): ");
+                input = Console.ReadLine();
+                input = input.ToUpper();
+                if (input == "Y") do
+                    {
+                        Console.Write("How many sides should each die have?");
+                        string input2 = Console.ReadLine();
+                        var number = int.Parse(input2);
+                        var number2 = number;
+                        Dice(number);
+                        Console.WriteLine("Would you like to continue? (y/n)");
+                        response = Console.ReadLine();
+                        response = response.ToUpper();
+                    }
+                    while (response == "Y");
+
+
+            }
+        }
+        public static void Dice(int number)
+        {
+            Random rnd = new Random();
+            int dice = rnd.Next(1, number);
+            Console.WriteLine("Roll 1:");
+            Console.WriteLine(dice);
+            int dice2 = rnd.Next(1, number);
+            Console.WriteLine(dice2);
+        }
+    }
+}
